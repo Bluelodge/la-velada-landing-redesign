e76a63cd29b2899255cb1f8a95d3a657f7af9287
Bluelodge <thebluelodgedev@gmail.com>
2023-05-28 18:23:15 -0500

    First commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				boxing: ['Boxing', 'Adjusted Arial Narrow Fallback', 'sans-seirf']
+			}
+		},
+	},
+	plugins: [
+		require('tailwindcss-animated')
+	],
+}
